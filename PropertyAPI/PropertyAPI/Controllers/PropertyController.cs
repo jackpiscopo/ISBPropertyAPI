7bafb8033bcd2abe63da5d58949cccb02456921f
Jack Piscopo <jackpiscopo1999@gmail.com>
2024-05-02 17:26:13 +0200

    Initial commit with API

---
+++ PropertyAPI/PropertyAPI/Controllers/PropertyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Property>>> GetProperties()
+        {
+            return new List<Property>
+            {
+                new Property
+                {
+                    PropertyName = "3 Bedroom Apartment",
+                    PropertyAddress = "3 Road",
+                    Price = 300000f,
+                    RegistrationDate = DateTime.Today
+                }
+            };
+        }
+    }
+}
---
+++ PropertyAPI/PropertyAPI/Property.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyAPI
+{
+    public class Property
+    {
+        public int PropertyId { get; set; }
+        public string PropertyName { get; set; }
+        public string PropertyAddress { get; set; }
+        public float Price { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
